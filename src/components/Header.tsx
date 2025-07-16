144b5e7d4cc1c0ba2f5b8547c429bfbca09331b6
Raynell Rajeev <108294211+raynellrajeev@users.noreply.github.com>
2025-07-16 16:39:03 +0530

    header blur

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function Header({ clearMemory }: { clearMemory: () => void }) {
   return (
-    <header className="fixed w-full p-4 flex justify-between bg-neutral-900/95 backdrop-blur-4xl shadow-2xl z-10">
+    <header className="fixed w-full p-4 flex justify-between bg-neutral-900/90 backdrop-blur-sm shadow-2xl z-10">
       <div className=" text-white text-md px-4 py-2 font-bold">GURU</div>
       <button
         type="button"
